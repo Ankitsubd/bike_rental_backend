a91033266291ec93c3df44289a9d6eb5414d39c8
Ankit Subedi <ankitsbd.it@gmail.com>
2025-08-02 19:18:26 +0545

    backend againn

--- backend/settings_production.py
+++ backend/settings_production.py
@@ -19,12 +19,8 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', default='5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
